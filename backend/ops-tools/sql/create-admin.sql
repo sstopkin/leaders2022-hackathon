7161ad2f79e43a37fe93fcbfe3705cf448fdc385
Sergey Stopkin <sstopkin@issart.com>
2022-10-27 15:09:05 +0000

    Update create-admin.sql

--- backend/ops-tools/sql/create-admin.sql
+++ backend/ops-tools/sql/create-admin.sql
@@ -1,2 +1,5 @@
 INSERT INTO public.users ("id","email","firstName","lastName","isActive","password","role","description") VALUES
 	 ('4456d796-0448-4f03-82e4-a97652fe52cd','admin@example.com','First','Admin',true,'$2b$12$9bq2TtfK6ot0e0CVBF3OIentt3lB2HAn1YjeOLOQZrgHzoatwl4eq','admin','first admin');
+
+INSERT INTO public.users ("id","email","firstName","lastName","isActive","password","role","description") VALUES
+	 ('b958b952-beb5-4f3e-8c9f-1b4c69b6e066','user@example.com','First','User',true,'$2b$12$9bq2TtfK6ot0e0CVBF3OIentt3lB2HAn1YjeOLOQZrgHzoatwl4eq','user','first user');
