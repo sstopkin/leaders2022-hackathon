bed8f57c7704dad212224ccd527dc85c203f0979
mlepiavko <mlepiavko@issart.com>
2022-10-31 20:49:24 +0700

    add code

--- generator/app/services/ml/generator.py
+++ generator/app/services/ml/generator.py
@@ -1,5 +1,18 @@
-def generate_pathologies(original_dicom_bytes: bytes) -> bytes:
-    # TODO call ML function generating pathologies DICOM file instead of the draft
-    generated_dicom_bytes = original_dicom_bytes
+from io import BytesIO
 
-    return generated_dicom_bytes
+import pydicom
+
+from services.ml.src.generate import damage_dicoms
+
+
+def pydicom_to_bytes(dicom):
+    with BytesIO() as bi:
+        dicom.save_as(bi)
+        return bi.getvalue()
+
+
+def generate_pathologies(original_dicom_bytes: bytes, lung_part: int) -> list(bytes):
+    ds = pydicom.dcmread(BytesIO(original_dicom_bytes), force=True)
+    new_dicoms = damage_dicoms([ds], lung_part=lung_part)
+    dicom_bytes = [pydicom_to_bytes(dicom) for dicom in new_dicoms]
+    return dicom_bytes
